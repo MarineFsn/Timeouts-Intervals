7f82a7da08a9c1dca4b4dedfca27075fe2834806
fassma89@gmail.com <fassma89@gmail.com>
2023-05-02 12:20:28 +0200

    Exo 1 completed

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,17 @@
-let time = 0;
-let letters = ["W" , "I" , "L" , "S" , "O" , "N" , "-" , "2"];
-function typewriter () {
-    if (time < letters.length) {
-      console.log(letters[time]);
-      time++;
-    }
-  };
 
+function typewriter() {
+    let letters = ["W", "i", "l", "s", "o", "n", "-", "2"];
+    let index = 0;
+    let outputElement = document.getElementById("typewriter");
+    let intervalId = setInterval(() => {
+      outputElement.innerHTML += letters[index];
+      index++;
+  
+      if (index === letters.length) {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+  }
+  typewriter();
 
-//function typewriter () {
-//    letters.forEach (console.log);
-
-//};
-
-//setInterval(typewriter, 1000); 
-
-
+  
